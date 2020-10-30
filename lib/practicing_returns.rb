846d8cc2535b197b6965ec5e1724860af47497aa
ralegria77 <ralegria77@gmail.com>
2020-10-30 14:09:16 -0700

    commit tree c3f74d420600dcc0fe6a48a8cc32a56c34ce4ff3

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
+
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    binding.pry
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
-end
-
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" } 
+  collection
+end 
